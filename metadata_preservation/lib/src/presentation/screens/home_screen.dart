2dc6c6786a53217dd07ae4f590991a4416fac1c6
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-10-04 21:32:57 -0300

    Update home_screen.dart

--- metadata_preservation/lib/src/presentation/screens/home_screen.dart
+++ metadata_preservation/lib/src/presentation/screens/home_screen.dart
@@ -1,9 +1,9 @@
+import 'package:exif/exif.dart';
 import 'package:flutter/material.dart';
-import 'dart:typed_data';
-import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:metadata_preservation/src/utils/modal_picker_image.dart';
-import 'package:exif/exif.dart';
+import 'dart:io';
+import 'package:image/image.dart' as impImage;
 import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<int> qualityOptions = [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ];
+  List<int> qualityOptions = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
   int? selectedQuality = 100;
 
@@ -32,7 +32,11 @@ class _HomeScreenState extends State<HomeScreen> {
           _body,
           qualityDropdown(),
           _executar(),
-          _imageSelect != null ? Text( !imagemIgual ? 'Imagem não contem os mesmos metadados' : 'Imagem contem os mesmos metadados') : const SizedBox.shrink()
+          _imageSelect != null
+              ? Text(!imagemIgual
+                  ? 'Imagem não contem os mesmos metadados'
+                  : 'Imagem contem os mesmos metadados')
+              : const SizedBox.shrink()
         ],
       ),
     );
@@ -40,21 +44,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _executar() {
     return ElevatedButton(
-      onPressed: () async {
-        Uint8List? _return = _imageSelect != null ? await compressImageWithMetadata(_imageSelect!, selectedQuality ?? 100) : null;
+        onPressed: () async {
+          Uint8List segundaImagem = await compressImage(_imageSelect!, 10);
 
-        if(_imageSelect != null && _return != null) {
-          bool _valor = await compareExifData(_imageSelect!, _return);
-          setState(() {
-            imagemIgual = _valor;
-          });
-          
-        }
+          bool teste = await compareExifData(_imageSelect!, segundaImagem);
 
-        print(_return);
-      },
-      child: null
-    );
+          print(teste);
+        },
+        child: null);
   }
 
   Widget qualityDropdown() {
@@ -91,11 +88,14 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget get _body {
     return GestureDetector(
       onTap: () {
-        ModalPickerImage.exibeModal(context, (imageSelect) {
-           setState(() {
-             _imageSelect = imageSelect;
-           });
-        },);
+        ModalPickerImage.exibeModal(
+          context,
+          (imageSelect) {
+            setState(() {
+              _imageSelect = imageSelect;
+            });
+          },
+        );
       },
       child: Container(
         padding: const EdgeInsets.all(30),
@@ -118,34 +118,20 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Future<Uint8List> compressImage(Uint8List imageBytes, int quality) async {
-    final result = await FlutterImageCompress.compressWithList(
-      imageBytes,
-      quality: quality,
-    );
+  Future<Uint8List> compressImage(Uint8List imagePath, int quality) async {
+    final image = impImage.decodeImage(imagePath);
 
-    if (result.length < imageBytes.length) {
-      return Uint8List.fromList(result);
-    } else {
-      // Retorna a imagem original se a compressão não teve efeito
-      return imageBytes;
-    }
+    final compressedImage = impImage.encodeJpg(image!, quality: quality);
+
+    // Retorna a imagem com a qualidade reduzida (com os metadados)
+    return compressedImage;
   }
 
-  Future<Uint8List> compressImageWithMetadata(List<int> imageData, int quality) async {
-    final result = await FlutterImageCompress.compressWithList(
-      Uint8List.fromList(imageData),
-      quality: quality,
-      keepExif: true,
-    );
-
-    return result;
-  }
-
-  Future<bool> compareExifData(Uint8List originalImageData, Uint8List compressedImageData) async {
+  Future<bool> compareExifData(
+      Uint8List originalImageData, Uint8List compressedImageData) async {
     final originalExifData = await readExifFromBytes(originalImageData);
     final compressedExifData = await readExifFromBytes(compressedImageData);
 
     return originalExifData.toString() == compressedExifData.toString();
-}
+  }
 }
